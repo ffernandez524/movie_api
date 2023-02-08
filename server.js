31c79a1ca6e1b4744549317c162f25986ca06980
ffernandez524 <flareniel@gmail.com>
2023-02-08 11:28:01 -0500

    Change content from paint text to html
    Change content from paint text to html

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ http.createServer((request, response) => {
     if(err){
       throw err;
     }
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(data);
     response.end('Hello Node!\n');    
   });  
