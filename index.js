59d2b26c75d84ff4e0bac85a88caab5626809d9c
ffernandez524 <flareniel@gmail.com>
2023-04-24 16:51:26 -0400

    Added GET path for a list of users

--- index.js
+++ index.js
@@ -64,6 +64,16 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) 
   });
 });
 
+//GET List of Users
+app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.find().then ((users) => { 
+    res.status(201).json(users);
+  }).catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+});
+
 //GET data of a single movie
 app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.findOne({ Title : req.params.Title }).then(movie => {
